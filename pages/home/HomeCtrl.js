d9112417cf7ad188bee70a0d90d4f39c1df0ad43
Florent Gouget <florent.gouget@gmail.com>
2015-09-19 10:33:02 +0200

    init

---
+++ pages/home/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+*/
+
+'use strict';
+
+angular.module('myApp').controller('HomeCtrl', ['$scope', 'zlQueryBuilder', function($scope, zlQueryBuilder) {
+	//TODO - put any directive code here
+}]);
